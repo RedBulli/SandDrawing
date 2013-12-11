513e417d5bb29b1e33fcfb18e45265c360194f0e
Laura Sirola <laura.sirola@aalto.fi>
2013-12-11 11:37:57 +0200

    Update FingerSize.js

--- FingerSize.js
+++ FingerSize.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     window.addEventListener("keydown", changeSize);
     
     function changeSize(e) {
-    	if(e.keyCode == 49) {
+    	if(e.keyCode == 50) {
     		FINGERTIP_RADIUS += 1;
     	}
     	
-    	else if(e.keyCode == 50) {
+    	else if(e.keyCode == 49) {
     		FINGERTIP_RADIUS -= 1;
     	}
     }
